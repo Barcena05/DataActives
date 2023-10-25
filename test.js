2797e8fa380c53a3f03d0c38e7b74e0297f12379
Daniman30 <regulardani1@gmail.com>
2023-10-25 00:32:10 -0400

    El selector, solo falta que pase valores. Arreglo en json

--- test.js
+++ test.js
@@ -1,8 +1,4 @@
-fetch('data.json').then(response => response.json()).then(data => {
-    function SelectSex(sex){
-
-    }
-})
+import data from './data/data.json' assert { type: "json"};
 
 function crearGradiente(svgID, gradienteID, porcentaje){
     var svg = document.getElementById(svgID);
@@ -38,165 +34,153 @@ function crearGradiente(svgID, gradienteID, porcentaje){
     svg.appendChild(defs);
 }
 
-crearGradiente('futbol', 'F1g', 10);
-crearGradiente('basket', 'F2g', 20);
-crearGradiente('rugby7', 'F3g', 30)
-crearGradiente('handball', 'F4g', 40);
-crearGradiente('hockey', 'F5g', 50);
-crearGradiente('softball', 'F6g', 60)
-crearGradiente('voley', 'F7g', 70);
-crearGradiente('polo', 'F8g', 80);
-crearGradiente('baseball', 'F9g', 90)
+function SelectSex(sex){
+  var managersXsports = [];
+  var totals = [];
+  var i = -1;
+  for (var sport in data.sports) {
+    i++;
+    totals[i] = 0;
+    managersXsports[i] = 0;
+    if (sex === 'female' || sex === 'all') {
+      totals[i] += data.sports[sport].female.numbers_of_teams;
+      for (var j = 0; j < data.sports[sport].female.countries.length; j++) {
+        if (data.sports[sport].female.countries[j].manager_sex === "female") managersXsports[i]++;
+      }
+    }
+    if (sex === 'male' || sex === 'all') {
+      totals[i] += data.sports[sport].male.numbers_of_teams;
+      for (var j = 0; j < data.sports[sport].male.countries.length; j++) {
+        if (data.sports[sport].male.countries[j].manager_sex === "female") managersXsports[i]++;
+      }
+    }
+  }
+  var final = [];
+  for (let i = 0; i < managersXsports.length; i++) {
+    final[i] = (managersXsports[i] * 100) / totals[i];
+  }
+  return final;
+}
 
+
+//Dos formas que investigue de como podia funcionar que el selector
+//le pasara los valores al resto de clases
+
+function FillAll() {
+  var x = document.getElementById("Sexo").value;
+  alert("El valor seleccionado es: " + x);
+  var a = SelectSex(x);
+  crearGradiente('futbol', 'F1g', a[1]);
+  crearGradiente('basket', 'F2g', a[0]);
+  crearGradiente('rugby7', 'F3g', a[2]);
+  crearGradiente('handball', 'F4g', a[4]);
+  crearGradiente('hockey', 'F5g', a[6]);
+  crearGradiente('softball', 'F6g', a[5]);
+  crearGradiente('voley', 'F7g', a[8]);
+  crearGradiente('polo', 'F8g', a[7]);
+  crearGradiente('baseball', 'F9g', a[3]);
+}
+
+//FillAll()
+
+const selectElement = document.querySelector(".Sexo");
+
+selectElement.addEventListener("change", (event) => {
+  var a = SelectSex(event.target.value);
+  alert("El valor seleccionado es: " + event.target.value);
+  crearGradiente('futbol', 'F1g', a[1]);
+  crearGradiente('basket', 'F2g', a[0]);
+  crearGradiente('rugby7', 'F3g', a[2]);
+  crearGradiente('handball', 'F4g', a[4]);
+  crearGradiente('hockey', 'F5g', a[6]);
+  crearGradiente('softball', 'F6g', a[5]);
+  crearGradiente('voley', 'F7g', a[8]);
+  crearGradiente('polo', 'F8g', a[7]);
+  crearGradiente('baseball', 'F9g', a[3]);
+
+  
+});
 
 window.onload = function() {
-    var svg1 = document.getElementById('basket');
-    var svg1b = document.getElementById('basketball');
-    var svg2 = document.getElementById('futbol');
-    var svg2b = document.getElementById('soccerball');
-    var svg3 = document.getElementById('rugby7');
-    var svg3b = document.getElementById('rugbyball');
-    var svg4 = document.getElementById('handball');
-    var svg4b = document.getElementById('handballball');
-    var svg5 = document.getElementById('hockey');
-    var svg5b = document.getElementById('hockeyball');
-    var svg6 = document.getElementById('softball');
-    var svg6b = document.getElementById('softballball');
-    var svg7 = document.getElementById('voley');
-    var svg7b = document.getElementById('voleyball');
-    var svg8 = document.getElementById('polo');
-    var svg8b = document.getElementById('poloball');
-    var svg9 = document.getElementById('baseball');
-    var svg9b = document.getElementById('baseballball');
+  var svg1 = document.getElementById('basket');
+  var svg1b = document.getElementById('basketball');
+  var svg2 = document.getElementById('futbol');
+  var svg2b = document.getElementById('soccerball');
+  var svg3 = document.getElementById('rugby7');
+  var svg3b = document.getElementById('rugbyball');
+  var svg4 = document.getElementById('handball');
+  var svg4b = document.getElementById('handballball');
+  var svg5 = document.getElementById('hockey');
+  var svg5b = document.getElementById('hockeyball');
+  var svg6 = document.getElementById('softball');
+  var svg6b = document.getElementById('softballball');
+  var svg7 = document.getElementById('voley');
+  var svg7b = document.getElementById('voleyball');
+  var svg8 = document.getElementById('polo');
+  var svg8b = document.getElementById('poloball');
+  var svg9 = document.getElementById('baseball');
+  var svg9b = document.getElementById('baseballball');
 
-    svg1.addEventListener('click', function(){
-      svg1b.style.rotate = '360deg';
+  svg1.addEventListener('click', function(){
+    svg1b.style.rotate = '360deg';
+    setTimeout(function(){
+      svg1b.style.rotate = '0deg';;
+      }, 500);
+  });
+
+  svg2.addEventListener('click', function(){
+      svg2b.style.rotate = '360deg';
       setTimeout(function(){
-        svg1b.style.rotate = '0deg';;
+        svg2b.style.rotate = '0deg';;
         }, 500);
   });
 
-    var porciento = '0%';
-    svg1.setAttribute('percentage', 'porciento')
-
-    svg2.addEventListener('click', function(){
-        svg2b.style.rotate = '360deg';
-        setTimeout(function(){
-          svg2b.style.rotate = '0deg';;
-          }, 500);
-    });
-
-    svg3.addEventListener('click', function(){
-        svg3b.style.rotate = '360deg';
-        setTimeout(function(){
-          svg3b.style.rotate = '0deg';;
-          }, 500);
-    });
-
-    svg4.addEventListener('click', function(){
-        svg4b.style.rotate = '360deg';
-        setTimeout(function(){
-          svg4b.style.rotate = '0deg';;
-          }, 500);
-    });
-
-    svg5.addEventListener('click', function(){
-      svg5b.style.top = "150px";
+  svg3.addEventListener('click', function(){
+      svg3b.style.rotate = '360deg';
       setTimeout(function(){
-        svg5b.style.top = "210px";
+        svg3b.style.rotate = '0deg';;
         }, 500);
   });
 
-    svg6.addEventListener('click', function(){
-        svg6b.style.rotate = '360deg';
-        setTimeout(function(){
-          svg6b.style.rotate = '0deg';;
-          }, 500);
-    });
+  svg4.addEventListener('click', function(){
+      svg4b.style.rotate = '360deg';
+      setTimeout(function(){
+        svg4b.style.rotate = '0deg';;
+        }, 500);
+  });
 
-    svg7.addEventListener('click', function(){
-        svg7b.style.rotate = '360deg';
-        setTimeout(function(){
-          svg7b.style.rotate = '0deg';;
-          }, 500);
-    });
+  svg5.addEventListener('click', function(){
+    svg5b.style.top = "180px";
+    setTimeout(function(){
+      svg5b.style.top = "240px";
+      }, 500);
+  });
 
-    svg8.addEventListener('click', function(){
-        svg8b.style.rotate = '360deg';
-        setTimeout(function(){
-          svg8b.style.rotate = '0deg';;
-          }, 500);
-    });
+  svg6.addEventListener('click', function(){
+      svg6b.style.rotate = '360deg';
+      setTimeout(function(){
+        svg6b.style.rotate = '0deg';;
+        }, 500);
+  });
 
-    svg9.addEventListener('click', function(){
-        svg9b.style.rotate = '360deg';
-        setTimeout(function(){
-          svg9b.style.rotate = '0deg';;
-          }, 500);
-    });
+  svg7.addEventListener('click', function(){
+      svg7b.style.rotate = '360deg';
+      setTimeout(function(){
+        svg7b.style.rotate = '0deg';;
+        }, 500);
+  });
+
+  svg8.addEventListener('click', function(){
+      svg8b.style.rotate = '360deg';
+      setTimeout(function(){
+        svg8b.style.rotate = '0deg';;
+        }, 500);
+  });
+
+  svg9.addEventListener('click', function(){
+      svg9b.style.rotate = '360deg';
+      setTimeout(function(){
+        svg9b.style.rotate = '0deg';;
+        }, 500);
+  });
 };
-
-
-
-// {
-//   "countries": {
-//       "ang": {
-//           "country_name": "Angola",
-//           "continent": "África",
-//           "development": ""
-//       },
-//     },
-//     "sports": {
-//         "basket": {
-//             "sport_name": "Baloncesto",
-//             "female": {
-//                 "event_name": "Copa Mundial",
-//                 "event_date": 2022,
-//                 "numbers_of_teams": 12,
-//                 "countries": [
-//                     {
-//                         "country_id": "aus",
-//                         "manager_name": "Sandy Brondello",
-//                         "manager_sex": "female",
-//                         "result": 3
-//                     },
-//                   ]
-//                 },
-//                 "male": {
-//                     "event_name": "Copa Mundial",
-//                     "event_date": "2023",
-//                     "countries": [
-//                         {
-//                             "country_id": "ger",
-//                             "manager_name": "Gordie Herbert",
-//                             "manager_sex": "male",
-//                             "result": 1
-//                         },
-//                       ]
-//                     }
-//                 },
-//                 "futbol": {
-//                     "sport_name": "Fútbol",
-//                     "female": {
-//                         "event_name": "Copa Mundial",
-//                         "event_date": 2023,
-//                         "numbers_of_teams": 32,
-//                         "countries": [
-//                             {
-//                                 "country_id": "aus",
-//                                 "manager_name": "Tony Gustavsson",
-//                                 "manager_sex": "male",
-//                                 "result": 4
-//                             },
-//                           ]
-//                         },
-//                         "male": {
-//                             "event_name": "Copa del Mundo",
-//                             "event_date": 2022,
-//                             "countries": [
-//                                 {
-//                                     "country_id": "aus",
-//                                     "manager_name": "Graham Arnold",
-//                                     "manager_sex": "male",
-//                                     "result": 11
-//                                 },
